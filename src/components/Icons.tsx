878017f2da763e737b672a93f5155c06f6b1f487
Bryan Jennings <bryanjenningz@gmail.com>
2023-05-25 20:17:17 -0700

    Move icons to their own file

---
+++ src/components/Icons.tsx
@@ -0,0 +1,87 @@
+export const MicrophoneSvg = () => {
+  return (
+    <svg height="24" viewBox="0 -960 960 960" width="24">
+      <path
+        fill="currentColor"
+        d="M479.972-389q-53.798 0-91.429-37.65-37.63-37.651-37.63-91.437v-238.326q0-53.786 37.658-91.437 37.659-37.65 91.457-37.65 53.798 0 91.429 37.65 37.63 37.651 37.63 91.437v238.326q0 53.786-37.658 91.437Q533.77-389 479.972-389Zm-43.559 283.587v-123.956q-108.065-14.479-178.457-96.855-70.391-82.376-70.391-191.863h87.413q0 85.152 59.983 145.087 59.983 59.935 145.087 59.935 85.104 0 145.039-59.969 59.935-59.969 59.935-145.053h87.413q0 109.544-70.391 191.892-70.392 82.347-178.457 96.826v123.956h-87.174Z"
+      />
+    </svg>
+  );
+};
+
+export const StopSvg = () => {
+  return (
+    <svg height="24" viewBox="0 -960 960 960" width="24">
+      <path
+        fill="currentColor"
+        d="M231.869-231.869v-496.262h496.262v496.262H231.869Z"
+      />
+    </svg>
+  );
+};
+
+export const PlaySvg = () => {
+  return (
+    <svg height="24" viewBox="0 -960 960 960" width="24">
+      <path
+        fill="currentColor"
+        d="M311.869-185.413v-589.174L775.066-480 311.869-185.413Z"
+      />
+    </svg>
+  );
+};
+
+export const SaveSvg = () => {
+  return (
+    <svg height="24" viewBox="0 -960 960 960" width="24">
+      <path
+        fill="currentColor"
+        d="M840-680v480q0 33-23.5 56.5T760-120H200q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h480l160 160ZM480-240q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35ZM240-560h360v-160H240v160Z"
+      />
+    </svg>
+  );
+};
+
+export const AudioSvg = () => {
+  return (
+    <svg height="24" viewBox="0 -960 960 960" width="24">
+      <path
+        fill="currentColor"
+        d="M560-131v-82q90-26 145-100t55-168q0-94-55-168T560-749v-82q124 28 202 125.5T840-481q0 127-78 224.5T560-131ZM120-360v-240h160l200-200v640L280-360H120Zm440 40v-322q47 22 73.5 66t26.5 96q0 51-26.5 94.5T560-320Z"
+      />
+    </svg>
+  );
+};
+
+export const FastRewindSvg = () => {
+  return (
+    <svg height="24" viewBox="0 -960 960 960" width="24">
+      <path
+        fill="currentColor"
+        d="M860-240 500-480l360-240v480Zm-400 0L100-480l360-240v480Z"
+      />
+    </svg>
+  );
+};
+
+export const FastForwardSvg = () => {
+  return (
+    <svg height="24" viewBox="0 -960 960 960" width="24">
+      <path
+        fill="currentColor"
+        d="M100-240v-480l360 240-360 240Zm400 0v-480l360 240-360 240Z"
+      />
+    </svg>
+  );
+};
+
+export const CloseSvg = () => {
+  return (
+    <svg height="24" viewBox="0 -960 960 960" width="24">
+      <path
+        fill="currentColor"
+        d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"
+      />
+    </svg>
+  );
+};
