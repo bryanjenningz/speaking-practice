8fe5ed4aae189ca30c9837f199f5059290d95027
Bryan Jennings <bryanjenningz@gmail.com>
2023-05-25 19:57:13 -0700

    Add styling and icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,6 +52,61 @@ const PlaySvg = () => {
   );
 };
 
+const SaveSvg = () => {
+  return (
+    <svg height="24" viewBox="0 -960 960 960" width="24">
+      <path
+        fill="currentColor"
+        d="M840-680v480q0 33-23.5 56.5T760-120H200q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h480l160 160ZM480-240q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35ZM240-560h360v-160H240v160Z"
+      />
+    </svg>
+  );
+};
+
+const AudioSvg = () => {
+  return (
+    <svg height="24" viewBox="0 -960 960 960" width="24">
+      <path
+        fill="currentColor"
+        d="M560-131v-82q90-26 145-100t55-168q0-94-55-168T560-749v-82q124 28 202 125.5T840-481q0 127-78 224.5T560-131ZM120-360v-240h160l200-200v640L280-360H120Zm440 40v-322q47 22 73.5 66t26.5 96q0 51-26.5 94.5T560-320Z"
+      />
+    </svg>
+  );
+};
+
+const FastRewindSvg = () => {
+  return (
+    <svg height="24" viewBox="0 -960 960 960" width="24">
+      <path
+        fill="currentColor"
+        d="M860-240 500-480l360-240v480Zm-400 0L100-480l360-240v480Z"
+      />
+    </svg>
+  );
+};
+
+const FastForwardSvg = () => {
+  return (
+    <svg height="24" viewBox="0 -960 960 960" width="24">
+      <path
+        fill="currentColor"
+        d="M100-240v-480l360 240-360 240Zm400 0v-480l360 240-360 240Z"
+      />
+    </svg>
+  );
+};
+
+const CloseSvg = () => {
+  return (
+    <svg height="24" viewBox="0 -960 960 960" width="24">
+      <path
+        fill="currentColor"
+        d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"
+      />
+    </svg>
+  );
+};
+
 const useClips = () => {
   const [clips, setClips] = useState<Clip[]>([]);
 
@@ -116,9 +171,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center bg-black text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 p-5">
+        <div className="container flex flex-col items-center justify-center gap-3 p-4">
           <form
-            className="flex w-full max-w-2xl"
+            className="relative flex w-full max-w-2xl"
             onSubmit={(e) => {
               e.preventDefault();
               const player = window.player;
@@ -127,69 +182,82 @@ const Home: NextPage = () => {
             }}
           >
             <input
-              className="grow bg-slate-700 px-4 py-2 text-white"
+              className="grow rounded-full bg-slate-700 py-2 pl-4 pr-10 text-white"
               value={videoId}
               onChange={(e) => setVideoId(e.target.value)}
               placeholder={`Video ID (e.g. ${DEFAULT_YOUTUBE_VIDEO_ID})`}
             />
-            <button className="bg-blue-700 px-4 py-2 uppercase text-white transition duration-300 hover:bg-blue-600">
-              Done
+            <button className="absolute bottom-0 right-0 top-0 flex items-center justify-center px-2 transition duration-300 hover:text-slate-300">
+              <PlaySvg />
+              <span className="sr-only">Search</span>
             </button>
           </form>
           <YouTubePlayer />
           <section className="flex flex-col gap-3">
             <section className="flex items-center gap-5">
               <button
-                className="bg-blue-700 px-4 py-2 uppercase text-white transition duration-300 hover:bg-blue-600"
+                className="flex flex-col items-center rounded-2xl bg-slate-700 px-4 py-2 uppercase text-white transition duration-300 hover:bg-slate-600"
                 onClick={() =>
                   setStartTime((startTime) => Math.max(0, startTime - 0.2))
                 }
-              >{`<`}</button>
+              >
+                <FastRewindSvg />
+                <span className="text-[8px]">{`-0.2 sec`}</span>
+              </button>
               <button
-                className="bg-blue-700 px-4 py-2 uppercase text-white transition duration-300 hover:bg-blue-600"
+                className="flex flex-col items-center rounded-2xl bg-slate-700 px-4 py-2 uppercase text-white transition duration-300 hover:bg-slate-600"
                 onClick={() => {
                   const player = window.player;
                   if (!player) return;
                   setStartTime(player.getCurrentTime());
                 }}
-                title="Start time"
               >
-                {formatVideoTime(startTime)}
+                <time>{formatVideoTime(startTime)}</time>
+                <span className="text-[8px]">Start time</span>
               </button>
               <button
-                className="bg-blue-700 px-4 py-2 uppercase text-white transition duration-300 hover:bg-blue-600"
+                className="flex flex-col items-center rounded-2xl bg-slate-700 px-4 py-2 uppercase text-white transition duration-300 hover:bg-slate-600"
                 onClick={() => setStartTime((startTime) => startTime + 0.2)}
-              >{`>`}</button>
+              >
+                <FastForwardSvg />
+                <span className="text-[8px]">{`+0.2 sec`}</span>
+              </button>
             </section>
 
             <section className="flex items-center gap-5">
               <button
-                className="bg-blue-700 px-4 py-2 uppercase text-white transition duration-300 hover:bg-blue-600"
+                className="flex flex-col items-center rounded-2xl bg-slate-700 px-4 py-2 uppercase text-white transition duration-300 hover:bg-slate-600"
                 onClick={() =>
                   setEndTime((endTime) => Math.max(0, endTime - 0.2))
                 }
-              >{`<`}</button>
+              >
+                <FastRewindSvg />
+                <span className="text-[8px]">{`-0.2 sec`}</span>
+              </button>
               <button
-                className="bg-blue-700 px-4 py-2 uppercase text-white transition duration-300 hover:bg-blue-600"
+                className="flex flex-col items-center rounded-2xl bg-slate-700 px-4 py-2 uppercase text-white transition duration-300 hover:bg-slate-600"
                 onClick={() => {
                   const player = window.player;
                   if (!player) return;
                   setEndTime(player.getCurrentTime());
                 }}
-                title="End time"
               >
-                {formatVideoTime(endTime)}
+                <time>{formatVideoTime(endTime)}</time>
+                <span className="text-[8px]">End time</span>
               </button>
               <button
-                className="bg-blue-700 px-4 py-2 uppercase text-white transition duration-300 hover:bg-blue-600"
+                className="flex flex-col items-center rounded-2xl bg-slate-700 px-4 py-2 uppercase text-white transition duration-300 hover:bg-slate-600"
                 onClick={() => setEndTime((endTime) => endTime + 0.2)}
-              >{`>`}</button>
+              >
+                <FastForwardSvg />
+                <span className="text-[8px]">{`+0.2 sec`}</span>
+              </button>
             </section>
           </section>
 
           <section className="flex items-center gap-5">
             <button
-              className="bg-blue-700 px-4 py-2 uppercase text-white transition duration-300 hover:bg-blue-600"
+              className="flex flex-col items-center rounded-2xl bg-slate-700 px-4 py-2 uppercase text-white transition duration-300 hover:bg-slate-600"
               onClick={() => {
                 const player = window.player;
                 if (!player) return;
@@ -200,10 +268,11 @@ const Home: NextPage = () => {
                 }, (endTime - startTime) * 1000);
               }}
             >
-              Play clip
+              <PlaySvg />
+              <span className="text-[8px]">Play clip</span>
             </button>
             <button
-              className="bg-blue-700 px-4 py-2 uppercase text-white transition duration-300 hover:bg-blue-600"
+              className="flex flex-col items-center rounded-2xl bg-slate-700 px-4 py-2 uppercase text-white transition duration-300 hover:bg-slate-600"
               onClick={() => {
                 saveClips((clips) => [
                   ...clips,
@@ -211,13 +280,14 @@ const Home: NextPage = () => {
                 ]);
               }}
             >
-              Save clip
+              <SaveSvg />
+              <span className="text-[8px]">Save clip</span>
             </button>
           </section>
 
           <section className="flex items-center gap-5">
             <button
-              className="bg-blue-700 px-4 py-2 uppercase text-white transition duration-300 hover:bg-blue-600"
+              className="flex flex-col items-center rounded-2xl bg-slate-700 px-4 py-2 uppercase text-white transition duration-300 hover:bg-slate-600"
               onClick={() => {
                 const audioRecorder = recorder.current;
                 if (!audioRecorder) {
@@ -249,17 +319,32 @@ const Home: NextPage = () => {
               {((): JSX.Element => {
                 switch (savedAudio.type) {
                   case "NO_AUDIO_SAVED":
-                    return <MicrophoneSvg />;
+                    return (
+                      <>
+                        <MicrophoneSvg />
+                        <span className="text-[8px]">Record</span>
+                      </>
+                    );
                   case "RECORDING_AUDIO":
-                    return <StopSvg />;
+                    return (
+                      <>
+                        <StopSvg />
+                        <span className="text-[8px]">Stop</span>
+                      </>
+                    );
                   case "RECORDED_AUDIO":
-                    return <MicrophoneSvg />;
+                    return (
+                      <>
+                        <MicrophoneSvg />
+                        <span className="text-[8px]">Record</span>
+                      </>
+                    );
                 }
               })()}
             </button>
 
             <button
-              className="bg-blue-700 px-4 py-2 uppercase text-white transition duration-300 hover:bg-blue-600 disabled:bg-slate-500"
+              className="flex flex-col items-center rounded-2xl bg-slate-700 px-4 py-2 uppercase text-white transition duration-300 hover:bg-slate-600 disabled:bg-slate-500"
               onClick={() => {
                 if (savedAudio.type !== "RECORDED_AUDIO") return;
                 savedAudio.play();
@@ -275,18 +360,23 @@ const Home: NextPage = () => {
                 }
               })()}
             >
-              <PlaySvg />
+              <AudioSvg />
+              <span className="text-[8px]">Listen</span>
             </button>
           </section>
 
-          <ul>
+          <ul className="flex w-full max-w-2xl flex-col gap-1">
             {clips.map((clip) => {
               const clipText = [clip.startTime, clip.endTime]
                 .map(formatVideoTime)
                 .join(" - ");
               return (
-                <li key={clip.id} className="flex items-center gap-3">
+                <li
+                  key={clip.id}
+                  className="flex items-center justify-between gap-3 rounded-full bg-slate-700 px-3 text-sm transition duration-300 hover:bg-slate-600"
+                >
                   <button
+                    className="grow py-2 text-left"
                     onClick={() => {
                       setStartTime(clip.startTime);
                       setEndTime(clip.endTime);
@@ -309,7 +399,7 @@ const Home: NextPage = () => {
                       )
                     }
                   >
-                    <span aria-hidden={true}>✖</span>
+                    <CloseSvg />
                     <span className="sr-only">Delete</span>
                   </button>
                 </li>
